bab91e69bb738d2d8923ae16f664e7c8874e667f
Barnegraver <andreasnilsen07@hotmail.com>
2025-06-18 18:11:24 +0200

    Added richer/more logging. Logs more of what/who the user is

--- logger.py
+++ logger.py
@@ -1,5 +1,11 @@
 import datetime
 
-def log_event(ip, user_agent, username, password):
+def log_event(ip, user_agent, action, detail="N/A", username=None, referrer=None, language=None):
+    timestamp = datetime.datetime.utcnow().isoformat()
+    log_entry = (
+        f"[{timestamp}] IP: {ip} | UA: {user_agent} | ACTION: {action} | "
+        f"USER: {username or 'Unknown'} | DETAIL: {detail} | "
+        f"REF: {referrer or 'N/A'} | LANG: {language or 'N/A'}\n"
+    )
     with open("honeypot.log", "a") as f:
-        f.write(f"[{datetime.datetime.now()}] IP: {ip}, UA: {user_agent}, USER: {username}, PASS: {password}\n")
+        f.write(log_entry)
